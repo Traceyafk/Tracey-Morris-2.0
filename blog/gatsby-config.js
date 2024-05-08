f4c30c8e462740fd067fcbcb056691ce8057de62
Tracey <morrist29@gmatc.matc.edu>
2024-05-08 15:13:55 -0500

    activty-2

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         spaceId: `xzjywthvriha`,
         accessToken: `6NutTHdmKaQLUIIzKNAaI_LeTELcLLeiPkjiHUVHwrY`
       }
-    }
+    },
     `gatsby-plugin-image`,
     {
       resolve: `gatsby-source-filesystem`,
