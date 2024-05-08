85021c49ee635e2e80b12a65284086f22f286025
Tracey <morrist29@gmatc.matc.edu>
2024-05-08 15:53:41 -0500

    actvity-2

---
+++ blog/src/templates/blog-post1.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+
+const BlogPost = ({ data }) => {
+    const { title } = data.contentfulBlogPost1;
+
+    return (
+        <Layout>
+            <h1>{title}</h1>
+        </Layout>
+    )
+}
+
+export default BlogPost1;
+
+export const pageQuery = graphql`
+    query blogPostQuery($slug: String!) {
+        contentfulBlogPost1(slug: {eq: $slug}) {
+            title
+            slug
+        }
+    }
+`
