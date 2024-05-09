694b02e520259311515a2f08be12a2c824dd002d
Tracey <morrist29@gmatc.matc.edu>
2024-05-09 01:37:20 -0500

    “activity-

--- blog/src/templates/blog-post1.js
+++ blog/src/templates/blog-post1.js
@@ -3,22 +3,30 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 
 const BlogPost1 = ({ data }) => {
-    const { title } = data.contentfulBlogPost1;
+    const { title, body } = data.contentfulBlogPost1;
 
     return (
         <Layout>
             <h1>{title}</h1>
+            <div dangerouslySetInnerHTML={{__html: body.childMarkdownRemark.html}}></div>
         </Layout>
-    )
+    );
 }
 
 export default BlogPost1;
 
 export const pageQuery = graphql`
     query blogPostQuery($slug: String!) {
-        contentfulBlogPost1(slug: {eq: $slug}) {
+        contentfulBlogPost(slug: {eq: $slug}) {
             title
             slug
-        }
+            body {
+                childMarkdownRemark {
+                    html
+                }
+            }
+	    }
     }
 `
+
+       
