56da3fe9f21db3252efa8155122fa253f72af679
Tracey <morrist29@gmatc.matc.edu>
2024-05-11 18:46:41 -0500

    activity-7

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -2,6 +2,9 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { Link } from "gatsby"
 import styled from "styled-components"
+import { H1 } from "../Heading"
+
+
 
 const StyledHeader = styled.header`
   margin: 0 auto;
@@ -20,7 +23,9 @@ color: ${props => props.theme.header.color};
 const Header = ({ siteTitle }) => (
   <StyledHeader>
     <StyledLink to="/">
+      <H1>
       {siteTitle}
+      </H1>
     </StyledLink>
   </StyledHeader>
 )
