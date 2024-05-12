1014c29c32e86feea8c307d3cf0f6b160ba77d56
Tracey <morrist29@gmatc.matc.edu>
2024-05-11 20:00:47 -0500

    activity-9

---
+++ blog/src/theme/Gray/Gray.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import mainHeaderImage from './images/react.png'
+import { Search } from 'styled-icons/feather'
+
+const images = {
+    mainHeaderimage
+}
+
+const icons = {
+    Search: <Search />
+}
+
+export const Gray = { ...theme, variants, images, icons }
