0e9b0cc294c8e8bfb19304eda28d8b56a86078c6
Tracey <morrist29@gmatc.matc.edu>
2024-05-11 20:29:13 -0500

    activity-9

--- blog/src/components/Button/SearchButton.js
+++ blog/src/components/Button/SearchButton.js
@@ -1,11 +1,11 @@
-import React from 'raect'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeConsumer } from 'styled-componenets'
+import { ThemeConsumer } from 'styled-components'
 import { IconButton } from './IconButton'
 
 export const SearchButton = props => (
     <ThemeConsumer>
-        {theme => <IconButton icon={theme.icons.Search} {...Props} />}
+        {theme => <IconButton icon={theme.icons.Search} {...props} />}
     </ThemeConsumer>
 )
 
