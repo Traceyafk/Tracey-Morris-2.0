83facfab8ae7c0470f2a321c5c59206c131b7190
Tracey <morrist29@gmatc.matc.edu>
2024-05-11 20:08:11 -0500

    activity-9

---
+++ blog/src/components/Button/SearchButton.js
@@ -0,0 +1,14 @@
+import React from 'raect'
+import propTypes from 'prop-types'
+import { ThemeConsumer } from 'styled-componenets'
+import { IconButton } from './IconButton'
+
+export const SearchButton = props => (
+    <ThemeConsumer>
+        {theme => <IconButton icon={theme.icons.Search} {...Props} />}
+    </ThemeConsumer>
+)
+
+SearchButton.propTypes = {
+    variant: PropTypes.string
+}
